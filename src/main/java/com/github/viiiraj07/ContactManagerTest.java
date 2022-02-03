8948c36d84b2afd896bccb75d45cda7f16346bc2
viiiraj07 <avisec07@gmail.com>
2022-02-03 22:49:27 +0530

    nested test

--- src/main/java/com/github/viiiraj07/ContactManagerTest.java
+++ src/main/java/com/github/viiiraj07/ContactManagerTest.java
@@ -12,7 +12,7 @@ class ContactManagerTest {
     ContactManager contactManager;
 
     @BeforeAll
-    void beforeAll(){
+    void beforeAll() {
         /*We don't need to mark it as static because we have changed the default test lifecycle
         by using @TestInstance(TestInstance.Lifecycle.PER_CLASS)
         */
@@ -20,63 +20,69 @@ class ContactManagerTest {
     }
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         System.out.println("Before each test cases");
     }
 
     @AfterAll
-    void afterAll(){
+    void afterAll() {
         System.out.println("After all test cases");
     }
 
     @Test
     @DisplayName("Should Throw Exception when null is passed")
     void addContact() {
-        Assertions.assertThrows(Exception.class, ()->{
+        Assertions.assertThrows(Exception.class, () -> {
             contactManager.addContact("ABhishek", "Kumar", null);
         });
     }
 
     @Test
     @DisplayName("Check if contact list is empty")
-    void checkList(){
+    void checkList() {
         Assertions.assertTrue(contactManager.contactList.isEmpty());
     }
 
     @Test
     @DisplayName("Throw exception for invalid phone number")
-    void validatePhoneNumber(){
-        Assertions.assertThrows(RuntimeException.class, ()->{
+    void validatePhoneNumber() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
             Contact contact = new Contact("Abbb", "Kkk", "3535fafs2asf525");
             contact.validatePhoneNumber();
         });
     }
 
     @RepeatedTest(value = 10)
-    void loopingTest(){
+    void loopingTest() {
         System.out.println("This will be printed multiple times");
     }
 
-    @ParameterizedTest
-    @CsvSource({"4172417294", "0124124124", "12412412"})
-    void checkNumbers(String phoneNumber){
-        Assertions.assertThrows(RuntimeException.class, ()->{
-            Contact contact = new Contact("Abbb", "Kkk", phoneNumber);
-            contact.validatePhoneNumber();
-        });
-    }
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class PhoneNumberTest {
+        @Disabled
+        @ParameterizedTest
+        @CsvSource({"4172417294", "0124124124", "12412412"})
+        void checkNumbers(String phoneNumber) {
+            Assertions.assertThrows(RuntimeException.class, () -> {
+                Contact contact = new Contact("Abbb", "Kkk", phoneNumber);
+                contact.validatePhoneNumber();
+            });
+        }
 
-    @DisplayName("Check Numbers Using MethodSource")
-    @ParameterizedTest
-    @MethodSource("listOfPhoneNumbers")
-    void checkNumbersUsingMethodSource(String phoneNumber){
-        Assertions.assertThrows(RuntimeException.class, ()->{
-            Contact contact = new Contact("Abbb", "Kkk", phoneNumber);
-            contact.validatePhoneNumber();
-        });
-    }
+        @DisplayName("Check Numbers Using MethodSource")
+        @ParameterizedTest
+        @MethodSource("listOfPhoneNumbers")
+        void checkNumbersUsingMethodSource(String phoneNumber) {
+            Assertions.assertThrows(RuntimeException.class, () -> {
+                Contact contact = new Contact("Abbb", "Kkk", phoneNumber);
+                contact.validatePhoneNumber();
+            });
+        }
 
-    String[] listOfPhoneNumbers(){
-        return new String[]{"4172417294", "0124124124", "12412412"};
+        String[] listOfPhoneNumbers() {
+            return new String[]{"4172417294", "124124124", "12412412"};
+        }
+
     }
 }
